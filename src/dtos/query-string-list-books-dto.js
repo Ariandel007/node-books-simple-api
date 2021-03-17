7e65517ec5f6eb1442614356b78c73e9ac4fd860
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2021-03-17 15:59:18 -0500

    limit dto modified

--- src/dtos/query-string-list-books-dto.js
+++ src/dtos/query-string-list-books-dto.js
@@ -11,10 +11,7 @@ class QueryStringListBooksDto {
             }
             if (query.limit != null) {
                 this.limit = parseInt(query.limit.trim()) < 30 ? parseInt(query.limit.trim()) : 30;
-            } else {
-                this.limit = 30;
             }
-
             if (query.sortBy) {
                 this.sortBy = query.sortBy.trim();
             }
